e9bc7f5f3c8c71c7c9fa378e3420cf4814254bc1
Teusner <quentin.brateau@ensta-bretagne.org>
2023-02-20 17:05:59 +0100

    Update

---
+++ launch/riptide_ros2_control.launch.py
@@ -0,0 +1,53 @@
+from launch import LaunchDescription
+from launch_ros.actions.node import Node
+from launch.substitutions import PathJoinSubstitution, Command, FindExecutable
+from launch_ros.substitutions import FindPackageShare
+
+from launch.actions import TimerAction
+
+
+import os
+import yaml
+from ament_index_python.packages import get_package_share_directory
+
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    # Get URDF via xacro
+    prefix = "riptide_1"
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare("riptide_description"), "urdf", "riptide.urdf.xacro"]
+            ),
+            " ",
+            "prefix:=",
+            prefix,
+        ]
+    )
+
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare("riptide_bringup"),
+            "config",
+            "controller_manager.yaml",
+        ]
+    )
+
+    # Controller manager
+    controller_manager_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_description, robot_controllers],
+        namespace=prefix,
+        output="both",
+    )
+    ld.add_action(controller_manager_node)
+
+    return ld
