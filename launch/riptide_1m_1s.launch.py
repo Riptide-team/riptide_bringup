428e1045c4876ff0be7a7031284b6631fe676607
Teusner <quentin.brateau@ensta-bretagne.org>
2023-02-21 10:46:12 +0100

    Update

--- launch/riptide_1m_1s.launch.py
+++ launch/riptide_1m_1s.launch.py
@@ -17,14 +17,14 @@ def generate_launch_description():
     ld.add_action(navigation_node)
 
     # Pressure Broadcaster
-    ld.add_action(
-        Node(
-            package="controller_manager",
-            executable="spawner",
-            namespace=prefix,
-            arguments=["pressure_broadcaster", "--controller-manager", "/" + prefix + "/controller_manager", "--unload-on-kill"],
-        )
-    )
+    # ld.add_action(
+    #     Node(
+    #         package="controller_manager",
+    #         executable="spawner",
+    #         namespace=prefix,
+    #         arguments=["pressure_broadcaster", "--controller-manager", "/" + prefix + "/controller_manager", "--unload-on-kill"],
+    #     )
+    # )
 
     # Depth Controller
     ld.add_action(
