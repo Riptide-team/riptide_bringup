366d65fcc321570715dad3ee1dc9cb6e5aba354d
Teusner <quentin.brateau@ensta-bretagne.org>
2023-02-21 09:46:33 +0100

    Update

---
+++ launch/riptide_1m_1s.launch.py
@@ -0,0 +1,39 @@
+from launch import LaunchDescription
+from launch_ros.actions.node import Node
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    # Get URDF via xacro
+    prefix = "riptide_1"
+
+    # Controller manager
+    navigation_node = Node(
+        package="riptide_navigation",
+        executable="001-1s_1m",
+        namespace=prefix,
+        output="both",
+    )
+    ld.add_action(navigation_node)
+
+    # Pressure Broadcaster
+    ld.add_action(
+        Node(
+            package="controller_manager",
+            executable="spawner",
+            namespace=prefix,
+            arguments=["pressure_broadcaster", "--controller-manager", "/" + prefix + "/controller_manager", "--unload-on-kill"],
+        )
+    )
+
+    # Depth Controller
+    ld.add_action(
+        Node(
+            package="controller_manager",
+            executable="spawner",
+            namespace=prefix,
+            arguments=["depth_controller", "--controller-manager", "/" + prefix + "/controller_manager", "--unload-on-kill"],
+        )
+    )
+
+    return ld
